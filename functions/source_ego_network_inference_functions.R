aec0bd9d902d48455d109c7e8befc5a44189e09a
Jeffrey A. Smith <69323464+JeffreyAlanSmith@users.noreply.github.com>
2022-06-19 08:45:45 -0500

    Add files via upload

--- functions/source_ego_network_inference_functions.R
+++ functions/source_ego_network_inference_functions.R
@@ -1,19 +1,18 @@
+url1 <- "https://github.com/JeffreyAlanSmith/ego_network_inference/raw/main/functions/"
 
-url1 <- "https://github.com/JeffreyAlanSmith/ego_network_inference/raw/master/functions"
-
-source(paste(url1, "adjusted.coefs.function.txt", sep = "")
-source(paste(url1, "calculate_network_statistics.txt", sep = "")
-source(paste(url1, "collect_best_coefficients", sep = "")
-source(paste(url1, "cutofffunc_netsfunc.txt", sep = "")
-source(paste(url1, "ego_rand_dist_functions_sample_igraph.txt", sep = "")
-source(paste(url1, "egofunc.txt", sep = "")
-source(paste(url1, "estimate_initial_homophily_coefs.txt", sep = "")
-source(paste(url1, "get.inputs.updateparams.txt", sep = "")
-source(paste(url1, "prepare_case_control_logistic_data_function_create_vars.txt", sep = "")
-source(paste(url1, "prepare_case_control_logistic_data_function.txt", sep = "")
-source(paste(url1, "prepare_ego_net_data_function.txt", sep = "")
-source(paste(url1, "run_netlist_function_alphavary_parallel.txt", sep = "")
-source(paste(url1, "whole_network_function_alphavary_parallel.txt", sep = "")
+source(paste(url1, "adjusted.coefs.function.txt", sep = ""))
+source(paste(url1, "calculate_network_statistics.txt", sep = ""))
+source(paste(url1, "collect_best_coefficients", sep = ""))
+source(paste(url1, "cutofffunc_netsfunc.txt", sep = ""))
+source(paste(url1, "ego_rand_dist_functions_sample_igraph.txt", sep = ""))
+source(paste(url1, "egofunc.txt", sep = ""))
+source(paste(url1, "estimate_initial_homophily_coefs.txt", sep = ""))
+source(paste(url1, "get.inputs.updateparams.txt", sep = ""))
+source(paste(url1, "prepare_case_control_logistic_data_function_create_vars.txt", sep = ""))
+source(paste(url1, "prepare_case_control_logistic_data_function.txt", sep = ""))
+source(paste(url1, "prepare_ego_net_data_function.txt", sep = ""))
+source(paste(url1, "run_netlist_function_alphavary_parallel.txt", sep = ""))
+source(paste(url1, "whole_network_function_alphavary_parallel.txt", sep = ""))
 
 
 
