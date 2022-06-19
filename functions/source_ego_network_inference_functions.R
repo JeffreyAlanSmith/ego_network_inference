612a551939624a3749f910af82b4b4828463cb05
Jeffrey A. Smith <69323464+JeffreyAlanSmith@users.noreply.github.com>
2022-06-19 08:52:39 -0500

    Add files via upload

--- functions/source_ego_network_inference_functions.R
+++ functions/source_ego_network_inference_functions.R
@@ -2,7 +2,7 @@ url1 <- "https://github.com/JeffreyAlanSmith/ego_network_inference/raw/main/func
 
 source(paste(url1, "adjusted.coefs.function.txt", sep = ""))
 source(paste(url1, "calculate_network_statistics.txt", sep = ""))
-source(paste(url1, "collect_best_coefficients", sep = ""))
+source(paste(url1, "collect_best_coefficients.txt", sep = ""))
 source(paste(url1, "cutofffunc_netsfunc.txt", sep = ""))
 source(paste(url1, "ego_rand_dist_functions_sample_igraph.txt", sep = ""))
 source(paste(url1, "egofunc.txt", sep = ""))
